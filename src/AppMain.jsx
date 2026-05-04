e76bbc4ac5b70318bdf3e1fb9c0d5d3d6d4bf8f3
Luis142009 <morenoluismiguel986@gmail.com>
2026-05-04 11:20:34 -0500

    funcional

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,16 +1,13 @@
-import React from "react";
-import{statusGame} from "./components/StatusGame.jsx";
+import React from 'react';
+import { StatusGame } from './components/StatusGame';
 
 export const AppMain = () => {
-
-<>
-<div>
- <h1>Hola vvs los odio a todos</h1>
-    <StatusGame />
-</div>
-
-
-</>
-
-
-}
+  return (
+    <>
+      <div>
+        <h1>Hola</h1>
+        <StatusGame />
+      </div>
+    </>
+  );
+};
