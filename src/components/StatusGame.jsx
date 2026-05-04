98f2599ea36cfaac33dc226e0b4a374d38f29724
Matheo <mt3oyepez@gmail.com>
2026-05-04 11:01:04 -0500

    h1

--- src/components/StatusGame.jsx
+++ src/components/StatusGame.jsx
@@ -7,16 +7,20 @@ export const StatusGame = () => {
   const [Puntos, setPuntos] = useState (0)
 
   const [Vidas, setVidas] = useState (0)
+
+
   return (
 
     <>
     <div className='Gemas'>
+      <h1>Gemas 🐧</h1>
       <button>Gemas 1</button>
       <button>Gemas 2</button>
       <button>Gemas 3</button>
     </div>
 
     <div className='puntos'>
+      <h1>Puntos 💫</h1>
         <button>+5</button>
         <button>+10</button>
         <button>15</button>
@@ -24,6 +28,7 @@ export const StatusGame = () => {
     </div>
 
     <div className='Vidas'>
+      <h1>Vidas ❤️</h1>
         <button>Vida 1</button>
         <button>Vida 2</button>
         <button>Vida 3</button>
