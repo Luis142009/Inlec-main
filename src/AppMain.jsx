c6d077a0e22f677147e8b6627a37596a4406776c
Luis142009 <morenoluismiguel986@gmail.com>
2026-05-06 11:21:47 -0500

    new vvs

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StatusGame } from './components/StatusGame';
 import { LuisRMPage } from './pages/LuisRMPage';
+import { MatheoRMPage} from './pages/MatheoRMPage';
+import { SneyderRMPage } from './pages/SneyderRMPage';
 
 export const AppMain = () => {
   return (
