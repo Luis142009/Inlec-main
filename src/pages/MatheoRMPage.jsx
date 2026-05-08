6e13b614a3cae091cc5a91617a88e569adbeed65
afsaanchez04 <afsanchez04@gmail.com>
2026-05-08 11:36:31 -0500

    tarjetas pokemon

--- src/pages/MatheoRMPage.jsx
+++ src/pages/MatheoRMPage.jsx
@@ -52,7 +52,7 @@ export const MatheoRMPage = () => {
       <h1>Personajes de Rick & Morty</h1>
 
       {Characters.map((char, index) => (
-        <div className="card" style={{ width: "10rem" }}>
+        <div key={index} className="card" style={{ width: "10rem" }}>
           <img src={char.image} className="card-img-top" alt="..."></img>
           <div className="card-body">
             <h5 className="card-title">{char.name}</h5>
@@ -64,7 +64,16 @@ export const MatheoRMPage = () => {
 
 
       <h1>Tarjetas Pokemon (Matheo)</h1>
-      
+      {pokemons.map((poke, index) => (
+        <div key={index} className="card" style={{ width: "10rem" }}>
+          <img src={poke.image} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <h5 className="card-title">{poke.name}</h5>
+            <p className="card-text"> Status: {poke.type}</p>
+            <p className="card-text"> Species: {poke.id}</p>
+          </div>
+        </div>
+      ))}
 
 
 
