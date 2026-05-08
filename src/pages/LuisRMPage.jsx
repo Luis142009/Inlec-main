a8d7cae4bdd38daaf5b646e247e9dd573f995e09
Luis142009 <morenoluismiguel986@gmail.com>
2026-05-08 11:32:55 -0500

    sofff

--- src/pages/LuisRMPage.jsx
+++ src/pages/LuisRMPage.jsx
@@ -16,9 +16,26 @@ const [Pokemon, setPokemon] = useState([0])
 
 const getPokemon = async () => {
 
-const res = await fetch("https://pokeapi.co/api/v2/pokemon/")
+const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12")
 const data = await res.json()
-setPokemon(data.results)
+
+const detallesPokemon = await Promise.all(
+  data.results.map(async (pokemon) => {
+    const res = await fetch(pokemon.url)
+    const detalles = await res.json()
+
+    return {
+      id: detalles.id,
+      name: detalles.name,
+      image: detalles.sprites.other["official-artwork"].front_default,
+      type: detalles.types[0].type.name
+    
+    }
+    
+  })
+)
+
+setPokemon(detallesPokemon)
 console.log(data)  
 
 }
@@ -35,7 +52,7 @@ useEffect(() => {
     <h1> Personajes de rick and Morty</h1>
    
       {character.map( (char, index) => (
-     <div className="card mb-3" style={{ maxWidth: "640px" }}>
+     <div key={index} className="card mb-3" style={{ maxWidth: "640px" }}>
   <div className="row g-0">
     <div className="col-md-4">
       <img src={char.image} className="img-fluid rounded-start" alt="..."></img>
@@ -53,7 +70,32 @@ useEffect(() => {
 
       ))}
     
-  
+    <h1> Tarjetas Pokemon </h1>
+
+<div clasName="container  ">
+  <div className="row gap-5 text-center">
+    {Pokemon.map( (char, index) => (
+     <div key={index} className="card mb-3" style={{ maxWidth: "640px" }}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src={char.image} className="img-fluid rounded-start" alt="..."></img>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">{char.name}</h5>
+        <p className="card-text">{char.type}</p>
+        <p className="card-text"><small className="text-body-secondary">{char.id} </small></p>
+        
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+      ))}
+       </div>
+        </div>
+    
    
 
    
