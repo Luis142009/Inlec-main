53024a38ccdd9ebb395b9a53958e1152c5721eb3
Luis142009 <morenoluismiguel986@gmail.com>
2026-05-22 10:52:25 -0500

    ver

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { StatusGame } from './components/StatusGame';
 import { LuisRMPage } from './pages/LuisRMPage';
 import { MatheoRMPage} from './pages/MatheoRMPage';
-import { SneyderRMPage } from './pages/SneyderRMPage';
 import InterfazCap from "./pages/interfazcap";
 
 export const AppMain = () => {
