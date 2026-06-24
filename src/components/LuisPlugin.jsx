4d1088ff14788fb1dc8ef2e5aaa2561c7a3d66ca
Luis142009 <morenoluismiguel986@gmail.com>
2026-06-24 11:26:04 -0500

    escena 1

--- src/components/LuisPlugin.jsx
+++ src/components/LuisPlugin.jsx
@@ -1,17 +1,17 @@
 import escena from "../assets/Escena.webm";
-import personaje from "../assets/experimentofox.webm";
+import personaje from "../assets/sra3.webm";
 import "../stylesheets/fondo.css";
 
 export const LuisPlugin = () => {
   return (
     <div className="screen">
-      {/* 🌌 FONDO */}
-      <video className="fondo" autoPlay loop muted playsInline>
+      
+      <video className="fondo"  loop muted playsInline>
         <source src={escena} type="video/webm" />
       </video>
 
-      {/* 🧍 PERSONAJE ENCIMA */}
-      <video className="personaje" autoPlay loop muted playsInline>
+    
+      <video className="personaje"  loop muted playsInline>
         <source src={personaje} type="video/webm" />
       </video>
     </div>
