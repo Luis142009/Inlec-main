bdff4a2dfd0445fd2629f4d4cdb93d3bde8ed760
Luis142009 <morenoluismiguel986@gmail.com>
2026-08-30 14:51:22 -0500

    NO hay nada que pueda hacer. Solo quiero el poder

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-import App from './App.jsx'
+import { AppMain } from './AppMain.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppMain />
   </StrictMode>,
 )
